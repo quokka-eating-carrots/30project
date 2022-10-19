3ffeb22418a9746e75182495677cc6ceb732062b
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-19 22:31:57 +0900

    Feat: Create dark theme, Change fonts

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import "../css/style.css";
+import { Keyboard } from "./keyboard.js";
 
-let a = 1
-let b = 2
-console.log( a + b ) 
+new Keyboard();
---
+++ src/js/keyboard.js
@@ -0,0 +1,23 @@
+export class Keyboard {
+  #switchEl;
+  #fontSelectEl;
+  constructor () {
+    this.#assignElement();
+    this.#addEvent();
+  }
+  #assignElement () {
+    this.#switchEl = document.getElementById("switch");
+    this.#fontSelectEl = document.getElementById("font");
+  }
+  #addEvent () {
+    this.#switchEl.addEventListener('change', (event) => {
+      document.documentElement.setAttribute(
+        "theme",
+        event.target.checked ? "dark-mode" : ""
+      );
+    });
+    this.#fontSelectEl.addEventListener("change", (event) => {
+      document.body.style.fontFamily = event.target.value;
+    })
+  }
+}
