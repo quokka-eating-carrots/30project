6a78a136d3dd29831fbe9356b068011ae2b14670
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-25 16:04:01 +0900

    Create image gallery

--- 30project/src/App.tsx
+++ 30project/src/App.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useCallback } from 'react';
+// import logo from './logo.svg';
+import { useDropzone } from 'react-dropzone'
 import './App.css';
+import ImageBox from './components/ImageBox';
 
 function App() {
+  const inpRef = useRef<HTMLInputElement>(null);
+
+  const [imageList, setImageList] = useState<string[]>([])
+
+  const onDrop = useCallback((acceptedFiles: string | any[]) => {
+    if (acceptedFiles.length) {
+      for (const file of acceptedFiles) {
+        const reader = new FileReader();
+  
+        reader.readAsDataURL(file)
+        reader.onloadend = (event) => {
+          setImageList(prev => [...prev, event.target?.result as string])
+        }
+      }
+    }
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
+  
   return (
     <div className="container">
-      <div className="initial-box">
-        <div className="text-center">
-          이미지가 없습니다. <br />
-          이미지를 추가해 주세요.
-        </div>
-        <div className="plus-box">
+      <div className={"gallery-box " + (imageList.length > 0 && 'row')}>
+        {
+          imageList.length === 0 &&
+          <div className="text-center">
+            이미지가 없습니다. <br />
+            이미지를 추가해 주세요.
+          </div>
+        }
+        {
+          imageList.map((el, idx) => <ImageBox key={el + idx} src={el} />)
+        }
+        <div className="plus-box"
+        {...getRootProps()}
+        >
+          <input type="file" ref={inpRef} 
+          {...getInputProps()}
+          />
           +
         </div>
       </div>
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ```
 $ npm i -global yarn
 $ yarn create react-app [폴더명] --template typescript
+$ yarn add @types/react-dropzone
 ```
